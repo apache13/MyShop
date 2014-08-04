41aa07b764a58201e2393766ee2d1a882b0489c9
apache13 <apache13@gmail.com>
2014-08-04 08:50:35 +0700

    add product image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def product_image_tag(product)
+    if(product != nil)
+        image_tag('/products/'+product.code+'.jpg' ,:class => "img-rounded center-block" , :title => product.name)
+    end
+
+  end
 end
